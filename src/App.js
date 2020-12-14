92d17d18cb47716e6ea6ef8b45ec9ee0654b567c
KumarAtmish <73941481+KumarAtmish@users.noreply.github.com>
2020-12-15 01:31:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Weather from "./Wether";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  );
+}
---
+++ src/Wether.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import "./Weather.css";
+
+const api = {
+  key: "2894e341d95bdc22ca9a6e4ed300b5bd",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
+
+function Weather() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "weather warm"
+            : "weather"
+          : "weather"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              {/* location */}
+              <div className="location">
+                {weather.name},{weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              {/* Round temprature */}
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              {/* change background image sunny or warm */}
+              <div className="weather"> {weather.weather[0].main} </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
+}
+export default Weather;
